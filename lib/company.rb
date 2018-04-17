752a517347947cffa09bebfe5312226542da331c
Tyler Westlie <tyler.westlie@gmail.com>
2018-04-17 11:56:39 -0600

    final commit

--- lib/company.rb
+++ lib/company.rb
@@ -16,32 +16,36 @@ class Company
   end
 
   def load_employees(filename)
-    CSV.open(filename, 'w') do |data|
-      @employees << data
+    CSV.foreach(filename) do |data|
+      employee_id = data[0]
+      name = data[1]
+      role = data[2]
+      start_date = data[3]
+      end_date = data[4]
+      employee = Employee.new(employee_id, name, role, start_date, end_date)
+      @employees << employee
     end
   end
-  #
-  # def load_employees(filename)
-  #   CSV.foreach(filename) do |data|
-  #     employee_id = data[0]
-  #     name = data[1]
-  #     role = data[2]
-  #     start_date = data[3]
-  #     end_date = data[4]
-  #     employee = Employee.new(employee_id, name, role, start_date, end_date)
-  #     @employees << employee
-  #   end
 
+  def load_projects(filename)
+    CSV.foreach(filename) do |data|
+      project_id = data[0]
+      name = data[1]
+      start_date = data[2]
+      end_date = data[3]
+      project = Project.new(project_id, name, start_date, end_date)
+      @projects << project
+    end
+  end
 
-  # def load_projects(filename)
-  #   CSV.foreach(filename, headers:true, header_converters: :symbol) do |data|
-  #     project = Project.new(data)
-  #     @projects[project.project_id] = project
-  # end
-  #
-  # def load_timesheets(filename)
-  #   CSV.foreach(filename, headers:true, header_converters: :symbol) do |data|
-  #     timesheet = TimeSheet.new(data)
-  #     @timesheets[timesheet.employee_id] = timesheet
-  # end
+  def load_timesheets(filename)
+    CSV.foreach(filename) do |data|
+      employee_id = data[0]
+      project_id = data[1]
+      date = data[2]
+      minutes = data[3]
+      timesheet = TimeSheet.new(employee_id, project_id, date, minutes)
+      @timesheets << timesheet
+    end
+  end
 end
--- test/company_test.rb
+++ test/company_test.rb
@@ -24,8 +24,21 @@ class CompanyTest < Minitest::Test
   def test_load_employees
     company = Company.new
     company_employees = company.load_employees('./data/employees.csv')
-    require 'pry'; binding.pry
 
-    assert_equal [], company_employees.employees
+    assert_equal "Susan Smith", company.employees.first.name
+  end
+
+  def test_load_projects
+    company = Company.new
+    company_projects = company.load_projects('./data/projects.csv')
+
+    assert_equal "Widgets", company.projects.first.name
+  end
+
+  def test_load_timesheets
+    company = Company.new
+    company_projects = company.load_timesheets('./data/timesheets.csv')
+
+    assert_equal 1, company.timesheets.first.employee_id
   end
 end
